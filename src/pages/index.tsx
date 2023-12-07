61fe4562e17da3959b1a5c9d765536ddd394ab00
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-12-07 16:10:05 +0100

    added instructions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,16 @@ export default function Home() {
   return (
     <main>
       <div className="flex items-center align-middle w-full justify-center h-screen mx-auto my-auto">
+        <div>
+
+          <h1 className="text-2xl">Sign in with Ethereum</h1>
+          <ul className="list">
+            <li>Connect your wallet</li>
+            <li>Click on the profile icon on the modal to sign in with Ethereum</li>
+            <li>Sign the message displayed in your wallet to verify you own the address</li>
+            <li>You are good to go!!!</li>
+          </ul>
+        </div>
         <ConnectKitButton />
       </div>
     </main>
